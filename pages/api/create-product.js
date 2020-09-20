7f2a5ca20b2b960ec51f12c3fa4b5cd60b1379ac
Zachary Townsend <zach@verbbrands.com>
2020-09-20 22:59:26 +0200

    feat(CMS): improve create product function

--- pages/api/create-product.js
+++ pages/api/create-product.js
@@ -3,27 +3,24 @@ import Axios from 'axios';
 import axios from 'axios';
 
 export default (req, res) => {
-    console.log({ req, res });
     const { body } = req;
-    axios({
-        method: 'post',
+    const options = {
+        method: 'POST',
         url: 'https://mapi.storyblok.com/v1/spaces/93371/stories/',
-        data: {
-            story: {
-                name: body.title,
-                slug: body.handle
-            }
-        },
         headers: {
-            Authorization: 'J5463PpgAwcqaam3OhCUWQtt-72710-FtuvwyJ9q5z-x11MwBjf',
-            'Content-Type': 'application/json'
-        }
-    })
-        .then((response) => {
-            console.log(response);
+            'content-type': 'application/json',
+            authorization: 'J5463PpgAwcqaam3OhCUWQtt-72710-FtuvwyJ9q5z-x11MwBjf'
+        },
+        data: { story: { name: body.title, slug: body.handle } }
+    };
+    axios
+        .request(options)
+        .then(function (response) {
+            console.log(response.data);
             res.status(200).send('it worked!');
         })
-        .catch((error) => {
-            res.status(200).send(error.message);
+        .catch(function (error) {
+            console.error(error);
+            res.status(422).send(error.message);
         });
 };
