4285c57d2dec975980583bea51a48d7f9e313b06
Zachary Townsend <zach@verbbrands.com>
2020-09-20 22:33:15 +0200

    feat(CMS): update create product endpoint

--- pages/api/create-product.js
+++ pages/api/create-product.js
@@ -1,4 +1,29 @@
+import Axios from 'axios';
+
+import axios from 'axios';
+
 export default (req, res) => {
     console.log({ req, res });
-    res.status(200).send(`Test!`);
+    const { body } = req;
+    axios({
+        method: 'post',
+        url: 'https://mapi.storyblok.com/v1/spaces/93371/stories/',
+        data: {
+            story: {
+                name: body.title,
+                slug: body.handle
+            }
+        },
+        headers: {
+            Authorization: 'J5463PpgAwcqaam3OhCUWQtt-72710-FtuvwyJ9q5z-x11MwBjf',
+            'Content-Type': 'application/json'
+        }
+    })
+        .then((response) => {
+            console.log(response);
+            res.status(200).send('it worked!');
+        })
+        .catch((error) => {
+            res.status(200).send(error.message);
+        });
 };
