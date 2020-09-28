5904ae9d2ec0f735b57d9e5600e97cd2a24ae8ed
Zachary Townsend <zach@verbbrands.com>
2020-09-28 23:57:12 +0200

    feat(HeroBlock): update heroblok with new schema

--- components/HeroBlock.js
+++ components/HeroBlock.js
@@ -7,15 +7,19 @@ const HeroBlock = ({ blok }) => {
     const resolver = new RichTextResolver();
     return (
         <SbEditable content={blok}>
-            <div className="relative w-full h-screen">
+            <div
+                className={`relative w-full h-screen flex justify-${blok.content_position_h} items-${blok.content_position_v} text-${blok.content_color} p-12`}
+                style={{ height: parseInt(blok.banner_height_desktop, 10) }}>
                 <img
                     src={blok.image.filename}
                     alt=""
                     className="absolute top-0 left-0 w-full h-full object-cover z-0"
                 />
-                <div className="z-10 relative">
+                <div className={`z-10 relative text-${blok.content_alignment}`}>
                     {parse(resolver.render(blok.content))}
-                    <a href={blok.button_url.cached_url}>{blok.button_text}</a>
+                    <a href={blok.cta_href.cached_url} target={blok.cta_target}>
+                        {blok.cta_text}
+                    </a>
                 </div>
             </div>
         </SbEditable>
