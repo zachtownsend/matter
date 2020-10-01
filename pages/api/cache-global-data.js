ab596cc07f7b41c9349fa4c830aa9af8bd004cdf
Zachary Townsend <zach@verbbrands.com>
2020-10-01 22:23:11 +0200

    feat(DataCache): add data cache endpoint

---
+++ pages/api/cache-global-data.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+import StoryblokService from '../../lib/storyblok-service';
+
+export default async (req, res) => {
+    console.log(req);
+    const { data } = await StoryblokService.get('cdn/stories', { cv: Date.now() });
+    const { stories } = data;
+    const component = stories.find((story) => (story.id === req.body.story_id ? story : false));
+
+    switch (component.slug) {
+        case 'global':
+            var globalData = JSON.stringify(component.content);
+            await fs.writeFileSync('data/global.json', globalData);
+            break;
+    }
+
+    res.status(200).send('caching!');
+};
